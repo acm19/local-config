9e1642c7bf113f7ed01f0ae87b5f6c01c65284db
acm <acm19@users.noreply.github.com>
2025-08-16 11:10:59 +0200

    Add autocompletion support
    Adds autocompletion support for both Go and Lua with all the base setup
    completed to add other languages easily.

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -9,15 +9,25 @@ return {
     "mason-org/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls" }
+        -- LSP configs: https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+        ensure_installed = { "lua_ls", "gopls", "golangci_lint_ls" }
       })
     end
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.gopls.setup({
+        capabilities = capabilities
+      })
+      lspconfig.golangci_lint_ls.setup({
+        capabilities = capabilities
+      })
 
       -- key bindings
       vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.hover, {})
