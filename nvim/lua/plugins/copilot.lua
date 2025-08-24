431da18ef14ca2c7f7b72ad5ee831d88482b0e83
acm <acm19@users.noreply.github.com>
2025-08-24 21:06:16 +0200

    Add Copilot, CodeCompanion, YAML parser support
    Adds Copilot and CodeCompanion plugins to improve coding assistance and
    AI-powered chat features in Neovim. Enables YAML parser in Treesitter to
    support YAML syntax highlighting and parsing. Updates the lockfile to
    ensure plugin versions are tracked.

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "github/copilot.vim",
+  },
+  {
+    "olimorris/codecompanion.nvim",
+    -- default config: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua#L12
+    config = function()
+      vim.keymap.set("n", "<leader>cc", ":CodeCompanionChat<CR>", { desc = "Code Companion" })
+      vim.keymap.set("n", "<leader>ca", ":CodeCompanionActions<CR>", { desc = "Code Companion Action Palette" })
+
+      require("codecompanion").setup({
+        display = {
+          chat = {
+            window = {
+              position = "right",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
