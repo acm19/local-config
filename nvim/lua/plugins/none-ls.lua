3ae535057e34530cd1de7b25a7fb462dbf4928a6
acm <acm19@users.noreply.github.com>
2025-10-03 18:56:50 +0200

    Add JSON formatting to nvim
    Adds JSON formatting support to nvim and updates all the plugins to the
    latest available version.

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
       -- update mason-lspconfig.lua to ensure the builtins are installed
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier, -- npm install -g prettier
       },
     })
 
