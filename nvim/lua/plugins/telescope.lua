16a9a5f46fdab155df8229b735fd103b1b1a3308
acm <acm19@users.noreply.github.com>
2025-12-11 20:59:25 +0100

    Add navidation keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,8 +8,12 @@ return {
       vim.keymap.set("n", "<A-S-o>", builtin.find_files, { desc = "Telescope find files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
       vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Telescope symbols" })
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+      vim.keymap.set('n', '<A-F7>', '<cmd>Telescope lsp_references<cr>', { desc = "Telescope LSP references" })
+      vim.keymap.set('n', '<A-I>', '<cmd>Telescope lsp_implementations<cr>', { desc = "Telescope LSP implementations" })
+      vim.keymap.set('n', '<A-D>', '<cmd>Telescope lsp_definitions<cr>', { desc = "Telescope LSP definitions" })
     end,
   },
   {
--- nvim/lua/vimrc.lua
+++ nvim/lua/vimrc.lua
@@ -7,3 +7,17 @@ vim.cmd("set ignorecase")
 vim.cmd("set smartcase")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+
+-- Keybindings sections --
+
+-- Move line up with Alt+Shift+K or Alt+Shift+Up
+vim.keymap.set('n', '<A-S-k>', ':m .-2<CR>==', { desc = 'Move line up' })
+vim.keymap.set('n', '<A-S-Up>', ':m .-2<CR>==', { desc = 'Move line up' })
+
+-- Move line down with Alt+Shift+J or Alt+Shift+Down
+vim.keymap.set('n', '<A-S-j>', ':m .+1<CR>==', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-S-Down>', ':m .+1<CR>==', { desc = 'Move line down' })
+
+-- Exit insert mode with 'jj' or 'jk'
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, desc = 'Exit insert mode' })
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, desc = 'Exit insert mode' })
