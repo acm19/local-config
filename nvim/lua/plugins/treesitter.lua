622caf72a4e43e666b037b5c02f8d40c232da789
acm <acm19@users.noreply.github.com>
2025-12-11 21:16:40 +0100

    Add symbol navigation
    Adds treesitter textobject to allow navigation across symbols. I'll test
    how useful it is and decide what keyboard shortcuts to keep.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,43 +1,111 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  branch = "master",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require"nvim-treesitter.configs".setup {
-      ensure_installed = { "c", "lua", "go", "gotmpl", "gomod", "gosum", "java", "vim", "vimdoc", "query", "markdown", "markdown_inline", "git_config", "gitignore", "make", "yaml", "htmldjango" },
-      sync_install = false,
-      auto_install = true,
-      ignore_install = { "javascript" },
-      highlight = {
-        enable = true,
-        disable = function(lang, buf)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "c",
+          "lua",
+          "go",
+          "gotmpl",
+          "gomod",
+          "gosum",
+          "java",
+          "vim",
+          "vimdoc",
+          "query",
+          "markdown",
+          "markdown_inline",
+          "git_config",
+          "gitignore",
+          "make",
+          "yaml",
+          "htmldjango",
+        },
+        sync_install = false,
+        auto_install = true,
+        ignore_install = { "javascript" },
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
-                return true
+              return true
             end
-        end,
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true }
-    }
+          end,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+      })
 
-    vim.filetype.add({
-      extension = {
-        gotmpl = 'gotmpl',
-      },
-      pattern = {
-        [".*/templates/.*%.tm?pl"] = "helm",
-        [".*/templates/.*%.ya?ml"] = "helm",
-        ["helmfile.*%.ya?ml"] = "helm",
-        -- Highlight Apache config files
-        [".*/apache2/.*%.conf"] = "apache",
-        [".*/httpd/.*%.conf"] = "apache",
-        ["httpd%.conf"] = "apache",
-        ["apache%.conf"] = "apache",
-        [".*/conf.d/.*%.conf"] = "apache",
-      },
-    })
-  end
+      vim.filetype.add({
+        extension = {
+          gotmpl = "gotmpl",
+        },
+        pattern = {
+          [".*/templates/.*%.tm?pl"] = "helm",
+          [".*/templates/.*%.ya?ml"] = "helm",
+          ["helmfile.*%.ya?ml"] = "helm",
+          -- Highlight Apache config files
+          [".*/apache2/.*%.conf"] = "apache",
+          [".*/httpd/.*%.conf"] = "apache",
+          ["httpd%.conf"] = "apache",
+          ["apache%.conf"] = "apache",
+          [".*/conf.d/.*%.conf"] = "apache",
+        },
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({})
+
+      -- keymaps
+      -- You can use the capture groups defined in `textobjects.scm`
+      vim.keymap.set({ "n", "x", "o" }, "]m", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]]", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+      end)
+      -- You can also pass a list to group multiple queries.
+      vim.keymap.set({ "n", "x", "o" }, "]o", function()
+        move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+      end)
+      -- You can also use captures from other query groups like `locals.scm` or `folds.scm`
+      vim.keymap.set({ "n", "x", "o" }, "]s", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@local.scope", "locals")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]z", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@fold", "folds")
+      end)
+
+      vim.keymap.set({ "n", "x", "o" }, "]M", function()
+        require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "][", function()
+        require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
+      end)
+
+      vim.keymap.set({ "n", "x", "o" }, "[m", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[[", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+      end)
+
+      vim.keymap.set({ "n", "x", "o" }, "[M", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[]", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
+      end)
+    end,
+  },
 }
