68bf401359ab6fe41826cfbbd5f2ad2649bf969b
acm <acm19@users.noreply.github.com>
2025-12-18 20:45:40 +0100

    Add symbol highlighting vis lsp
    Adds symbol highlighting while experimenting with a shorter update time
    for a snappy response, keep an eye on possible performance degradation
    elsewhere.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,8 @@
 require("vimrc")
 require("config.lazy")
 require("config.autocmds")
+
+-- Set a lower updatetime for better responsiveness.
+-- I making use of CursorHold events. I'll revistit
+-- if this causes performance issues.
+vim.opt.updatetime = 250
--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -27,6 +27,29 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      -- Highlighting the symbol under the cursor
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client.server_capabilities.documentHighlightProvider then
+            local group = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = false })
+            vim.api.nvim_clear_autocmds({ buffer = args.buf, group = group })
+
+            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+              buffer = args.buf,
+              group = group,
+              callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd("CursorMoved", {
+              buffer = args.buf,
+              group = group,
+              callback = vim.lsp.buf.clear_references,
+            })
+          end
+        end,
+      })
+
       vim.keymap.set({ "n", "i" }, "<C-k>", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "i" }, "<C-K>", vim.lsp.buf.implementation, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
