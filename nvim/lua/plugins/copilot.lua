b5e1615d8ba829993577328d1730787518d58cf2
acm <acm19@users.noreply.github.com>
2026-04-20 11:20:10 +0200

    Disable AI plugins
    Disables Copilot as I'm not using it at the moment. Removes avante as
    that was mostly a proof of concept. Updates all plugins.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,26 +1,26 @@
-return {
-  {
-    "github/copilot.vim",
-    config = function()
-      vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)")
-    end,
-  },
-  {
-    "olimorris/codecompanion.nvim",
-    -- default config: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua#L12
-    config = function()
-      vim.keymap.set("n", "<leader>cc", ":CodeCompanionChat<CR>", { desc = "Code Companion" })
-      vim.keymap.set("n", "<leader>cd", ":CodeCompanionActions<CR>", { desc = "Code Companion Action Palette" })
-
-      require("codecompanion").setup({
-        display = {
-          chat = {
-            window = {
-              position = "right",
-            },
-          },
-        },
-      })
-    end,
-  },
-}
+-- return {
+--   {
+--     "github/copilot.vim",
+--     config = function()
+--       vim.keymap.set("i", "<C-L>", "<Plug>(copilot-accept-word)")
+--     end,
+--   },
+--   {
+--     "olimorris/codecompanion.nvim",
+--     -- default config: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua#L12
+--     config = function()
+--       vim.keymap.set("n", "<leader>cc", ":CodeCompanionChat<CR>", { desc = "Code Companion" })
+--       vim.keymap.set("n", "<leader>cd", ":CodeCompanionActions<CR>", { desc = "Code Companion Action Palette" })
+--
+--       require("codecompanion").setup({
+--         display = {
+--           chat = {
+--             window = {
+--               position = "right",
+--             },
+--           },
+--         },
+--       })
+--     end,
+--   },
+-- }
